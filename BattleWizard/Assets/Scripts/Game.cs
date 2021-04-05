77108225bf9ee0074c6318184574afad38d0ef0f
Daniel Zheng <daniel.zheng@barnebys.com>
2021-04-05 16:49:46 +0200

    add fireball constellation

--- BattleWizard/Assets/Scripts/Game.cs
+++ BattleWizard/Assets/Scripts/Game.cs
@@ -13,14 +13,15 @@ public class Game : MonoBehaviour
 
     public GameObject manaPrefab;
     public GameObject starPrefab;
+    public GameObject targetPrefab;
     public ContactFilter2D contactFilter;
     
     private Mode mode = Mode.Move;
     private GameObject starObject;
     private List<GameObject> manaList = new List<GameObject>();
+    private List<GameObject> targetList = new List<GameObject>();
     private List<GameObject> hillConstellation = new List<GameObject>();
-
-    
+    private List<GameObject> fireballConstellation = new List<GameObject>();
     
     // Start is called before the first frame update
     void Start()
@@ -28,16 +29,24 @@ public class Game : MonoBehaviour
         var star1 = Instantiate(starPrefab, new Vector3(-1, 0, 0), this.transform.rotation);
         var star2 = Instantiate(starPrefab, new Vector3(0, +1, 0), this.transform.rotation);
         var star3 = Instantiate(starPrefab, new Vector3(+1, 0, 0), this.transform.rotation);
+        var star4 = Instantiate(starPrefab, new Vector3(0, 0, 0), this.transform.rotation);
+        var star5 = Instantiate(starPrefab, new Vector3(0, -1, 0), this.transform.rotation);
 
         hillConstellation.Add(star1);
         hillConstellation.Add(star2);
         hillConstellation.Add(star3);
+
+        fireballConstellation.Add(star1);
+        fireballConstellation.Add(star2);
+        fireballConstellation.Add(star3);
+        fireballConstellation.Add(star4);
+        fireballConstellation.Add(star5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (mode == Mode.Move && Input.GetMouseButtonDown(0))
         {
             // Debug.Log("mouse down:" + Input.mousePosition);
             var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
@@ -46,26 +55,40 @@ public class Game : MonoBehaviour
             
             if(hit.collider != null)
             {
-                Debug.Log("true:" + hit.point);
-                Debug.Log(hit.collider.gameObject.tag);
             }
 
             mode = Mode.Mana;
         }
 
-        if (Input.GetMouseButtonUp(0))
+        if (mode == Mode.Mana && Input.GetMouseButtonUp(0))
         {
-            mode = Mode.Move;
-
-            if(Constellation(hillConstellation))
+            if(Constellation(fireballConstellation))
+            {
+                Debug.Log("FireballSpell");
+                mode = Mode.Target;
+            }
+            else if(Constellation(hillConstellation))
             {
                 Debug.Log("HillSpell");
+                mode = Mode.Target;
+            }
+            else
+            {
+                mode = Mode.Move;
             }
 
             foreach(var manaObject in manaList)
             {
                 Destroy(manaObject);
             }
+        }
+
+        if (mode == Mode.Target && Input.GetMouseButtonDown(0))
+        {
+            mode = Mode.Move;
+            var p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var targetObject = Instantiate(targetPrefab, new Vector3(p.x, p.y, 0), this.transform.rotation);
+            targetList.Add(targetObject);
         }
 
         if(mode == Mode.Mana)
